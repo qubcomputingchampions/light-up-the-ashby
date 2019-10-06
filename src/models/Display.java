3ec66ad9ea4bbe93903eb6125337337294dc8489
Jamie Gardner <jamie.gardner@civica.co.uk>
2019-10-06 13:54:51 +0100

    Update display class

--- src/models/Display.java
+++ src/models/Display.java
@@ -4,6 +4,9 @@ public class Display {
     private int[][] pixels;
     private double refreshRate;
 
+    private final double pixelAspect = 0.8;
+    private final double displayAspect = 1.0;
+
     public Display(int[][] pixels, double refreshRate) {
         this.pixels = pixels;
         this.refreshRate = refreshRate;
@@ -23,5 +26,13 @@ public class Display {
 
     public void setRefreshRate(double refreshRate) {
         this.refreshRate = refreshRate;
+    }
+
+    public double getPixelAspect() {
+        return pixelAspect;
+    }
+
+    public double getDisplayAspect() {
+        return displayAspect;
     }
 }
